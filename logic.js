9b6047dbd19b8ef0f1b2a9f413a53cad78b363b3
gabrielcoder247 <gabrielcoder247@gmail.com>
2020-12-15 10:07:55 +0100

    ddebugging errors

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
-let daysOfPower = function get_days_of_power(){
+ function get_days_of_power(k){
 
 
-                function get_days_of_power_d1(R1, D1, D2 ){
+               return  function get_days_of_power(R1, D1, D2 ){
                     var ans = [];
                 for (let i = D1; i <= D2; i++) {
                     ans.push(i);
@@ -9,14 +9,14 @@ let daysOfPower = function get_days_of_power(){
                 let dayOfPower1 = ans.length
                 dayOfPower1 * R1;
 
-                return function get_days_of_power_d2(R2, D2, D3 ){
+                return function get_days_of_power(R2, D2, D3 ){
                     var ans = [];
                 for (let i = D2; i <= D3; i++) {
                     ans.push(i);
                 }
                 let dayOfPower2 = ans.length
                 dayOfPower2 * R2;
-                return  function get_days_of_power_d3(R3, D3, D4 ){
+                return  function get_days_of_power(R3, D3, D4 ){
                     var ans = [];
                 for (let i = D3; i <= D4; i++) {
                     ans.push(i);
@@ -24,6 +24,7 @@ let daysOfPower = function get_days_of_power(){
                 let dayOfPower3 = ans.length
                 dayOfPower3 * R3;
                 return dayOfPower1+dayOfPower2+dayOfPower3
+                // return D1+D2+D3
             }
 
         }
